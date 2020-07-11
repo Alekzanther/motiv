f504a411d104358399265237ca8d50849dc5c79c
Alexander Lindberg <alexanderlndbrg@gmail.com>
2020-07-11 14:58:07 +0200

    Add scraper.rs

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,14 @@
 #[macro_use]
 extern crate rocket;
 
+mod scraper;
+
 #[get("/")]
 fn hello() -> &'static str {
     "Hello, world!"
 }
 
 fn main() {
+    println!("visit dirs: {}", scraper::visit_dirs());
     rocket::ignite().mount("/", routes![hello]).launch();
 }
