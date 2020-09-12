3c2ef5fd6a2cd6eaeda4c3d9b30447ecef58a913
Alexander Lindberg <alexanderlndbrg@gmail.com>
2020-09-12 23:14:03 +0200

    Add theme stub and material ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,23 @@
 import React from "react";
 import logo from "./logo.svg";
+import { Button } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/core/styles";
 import "./App.css";
+import theme from "./theme/default.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>M O T I V</p>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            <Button color="secondary">l o c o</Button>
+          </p>
+          <p>M O T I V</p>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
