0ba7b8d07340fc26251cbfeea65e0f2d8cea52a4
Alexander Lindberg <alexanderlndbrg@gmail.com>
2020-09-12 21:52:05 +0200

    Add apollo graphql client

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          M O T I V E
-        </p>
+        <p>M O T I V</p>
       </header>
     </div>
   );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { gql } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+
+client
+  .query({
+    query: gql`
+      query {
+        allMedia {
+          id
+          path
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
