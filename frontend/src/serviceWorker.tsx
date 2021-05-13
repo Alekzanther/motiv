2bb4575bb55aec6d0859a5039a20f81889cacb3a
Alexander Lindberg <alexanderlndbrg@gmail.com>
2021-05-13 13:01:24 +0200

    Add comma rule and add exception to generated code

--- frontend/src/serviceWorker.tsx
+++ frontend/src/serviceWorker.tsx
@@ -9,6 +9,7 @@
 
 // To learn more about the benefits of this model and instructions on how to
 // opt-in, read https://bit.ly/CRA-PWA
+// eslint-ignore
 
 const isLocalhost = Boolean(
   window.location.hostname === "localhost"
