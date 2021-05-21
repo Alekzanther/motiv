b03deb8fa6cd7b88829a8e758cb3d677839642d8
Alexander Lindberg <alexanderlndbrg@gmail.com>
2021-05-21 23:04:57 +0200

    Remove unecessary imports

--- frontend/src/main/App.tsx
+++ frontend/src/main/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { ThemeProvider } from "@material-ui/core/styles";
 // import "./App.css";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
