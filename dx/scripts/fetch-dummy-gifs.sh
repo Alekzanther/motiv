99acc93fb5c880475ae20bda06d0a43f723d94b0
Alexander Lindberg <alexanderlndbrg@gmail.com>
2022-01-23 21:55:46 +0100

    Restructure DevEx-stuff into a dx-folder

--- dx/scripts/fetch-dummy-gifs.sh
+++ dx/scripts/fetch-dummy-gifs.sh
@@ -3,7 +3,7 @@ cd $1
 for i in {1..3}
 do
   timestamp=$(date +%s)
-  wget -O dummy.$timestamp.gif https://www.placecage.com/gif/640/480
+  wget -O dummy.$timestamp.gif https://www.placecage.com/gif/58$i/48$i
   sleep 1
 done
 exit
--- dx/scripts/fetch-dummy-videos.sh
+++ dx/scripts/fetch-dummy-videos.sh
@@ -3,13 +3,13 @@
 cd $1
 
 curl -O "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
-# "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"
-# "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4"
+curl -O "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"
+# curl -O "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4"
 # "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4"
 # "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4"
 # "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4"
 # "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4"
 # "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.jpg"
 # "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4"
-# "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4
+curl -O "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4
 exit
