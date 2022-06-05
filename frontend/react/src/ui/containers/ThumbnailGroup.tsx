11b8f2d3844e189ea389aa224e0201514830fb6f
Alexander Lindberg <alexanderlndbrg@gmail.com>
2022-06-05 23:10:09 +0200

    Fix aspect ratio (on box, still wrong for image)

--- frontend/react/src/ui/containers/ThumbnailGroup.tsx
+++ frontend/react/src/ui/containers/ThumbnailGroup.tsx
@@ -1,7 +1,7 @@
 import { MediaDisplayPropsFragment } from "../../queries/types/graphql";
 import Thumbnail from "../components/Thumbnail";
 import "react-lazy-load-image-component/src/effects/blur.css";
-import { SimpleGrid, Stack } from "@mantine/core";
+import { AspectRatio, SimpleGrid, Stack } from "@mantine/core";
 import MotivText from "../components/MotivText";
 
 const ThumbnailGroup = (props: {
@@ -17,19 +17,19 @@ const ThumbnailGroup = (props: {
         {title}
       </MotivText>
       <SimpleGrid
-        cols={4}
+        cols={5}
         spacing="lg"
         breakpoints={[
-          { maxWidth: 980, cols: 3, spacing: "md" },
+          { maxWidth: 980, cols: 4, spacing: "md" },
           { maxWidth: 755, cols: 2, spacing: "sm" },
           { maxWidth: 600, cols: 1, spacing: "sm" },
         ]}
       >
         {data
           && data.map((media) => (
-            <div key={media.id}>
+            <AspectRatio ratio={1} key={media.id}>
               <Thumbnail thumbnailClickedCallback={props.thumbnailClickedCallback} media={media}/>
-            </div>
+            </AspectRatio>
           ))}
       </SimpleGrid>
     </Stack>
