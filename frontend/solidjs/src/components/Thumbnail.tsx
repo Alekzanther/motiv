b92935d7cca16fc61cd4786c9a30e156beceea43
Alexander Lindberg <alexanderlndbrg@gmail.com>
2022-06-13 15:05:35 +0200

    Primitive thumbnail functionality

--- frontend/solidjs/src/components/Thumbnail.tsx
+++ frontend/solidjs/src/components/Thumbnail.tsx
@@ -1,5 +1,6 @@
-// import { createResource } from "solid-js";
 import { MediaDisplayPropsFragment } from "../queries/types/graphql";
+import { createViewportObserver } from "@solid-primitives/intersection-observer";
+import { createSignal } from "solid-js";
 
 export type ThumbnailProps = {
   media: MediaDisplayPropsFragment;
@@ -7,11 +8,27 @@ export type ThumbnailProps = {
 
 const Thumbnail = (props: ThumbnailProps) => {
   const { id, processed } = props.media;
-  // const [media] = createResource(() => {});
+  const [visible, setVisible] = createSignal(false);
+
+  const [observer] = createViewportObserver([], { threshold: 0.2 });
 
   const src = processed ? `/m/${id}/0` : `/m/${id}`;
-  //  return <div class={`bg-cover bg-[url('${src}')]`}></div>;
-  return <img src={src} />;
+  return (
+    <div
+      class={"aspect-square w-100% h-100%"}
+      use:observer={(e) => e.isIntersecting && setVisible(true)} //setVisible(true)}
+    >
+      {visible() ? (
+        <div
+          style={`background-image:url('${src}')`}
+          class={"bg-cover aspect-square w-100% h-100% "}
+        />
+      ) : (
+        <div class="text-xl">Not visible!</div>
+      )}
+    </div>
+  );
+  //return <img src={src} />;
 };
 
 export default Thumbnail;
