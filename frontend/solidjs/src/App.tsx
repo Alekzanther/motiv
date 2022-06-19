d9c4cf2feb24289819e4a8c8a042b2b922c4256e
Alexander Lindberg <alexanderlndbrg@gmail.com>
2022-06-19 23:01:24 +0200

    Add routing and update look and feel

--- frontend/solidjs/src/App.tsx
+++ frontend/solidjs/src/App.tsx
@@ -1,4 +1,5 @@
 import { createClient } from "@urql/core";
+import { Navigate, NavLink, Route, Routes } from "solid-app-router";
 import type { Component } from "solid-js";
 import Feed from "./pages/Feed";
 
@@ -6,51 +7,61 @@ const gqlClient = createClient({ url: "http://0.0.0.0:5000/graphql" });
 
 const App: Component = () => {
   return (
-    <div class="drawer">
-      <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
-      <div class="drawer-content flex flex-col">
-        {/* Navbar */}
-        <div class="w-full navbar bg-base-300">
-          <div class="flex-none lg:hidden">
-            <label for="my-drawer-3" class="btn btn-square btn-ghost">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                class="inline-block w-6 h-6 stroke-current"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                ></path>
-              </svg>
+    <div class="drawer drawer-mobile">
+      <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+      <div class="drawer-content flex flex-col ">
+        <div class="navbar sticky top-0 z-10 drop-shadow bg-base-100">
+          <div class="flex-1">
+            <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">
+              ...
             </label>
+            <a class="btn btn-ghost normal-case text-xl lg:hidden">Motiv</a>
           </div>
-          <div class="flex-1 px-2 mx-2 text-xl">Motiv</div>
-          <div class="flex-none hidden lg:block">
-            <ul class="menu menu-horizontal">
-              {/* Navbar menu */}
-              <li>
-                <a>Timeline</a>
-              </li>
-              <li>
-                <a>Albums</a>
-              </li>
-            </ul>
+          <div class="flex-none gap-2">
+            <div class="form-control">
+              <input type="text" placeholder="Search" class="input input-bordered" />
+            </div>
+            <div class="dropdown dropdown-end">
+              <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+                <div class="w-10 rounded-full">
+                  <img src="https://api.lorem.space/image/face?hash=33791" />
+                </div>
+              </label>
+              <ul
+                tabindex="0"
+                class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+              >
+                <li>
+                  <a class="justify-between">
+                    Profile
+                    <span class="badge">New</span>
+                  </a>
+                </li>
+                <li>
+                  <a>Settings</a>
+                </li>
+                <li>
+                  <a>Logout</a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-        <Feed client={gqlClient} />
+        <Routes>
+          <Route path="/">
+            <Navigate href="/feed" />
+          </Route>
+          <Route path="/feed" element={<Feed client={gqlClient} />} />
+        </Routes>
       </div>
       <div class="drawer-side">
-        <label for="my-drawer-3" class="drawer-overlay"></label>
-        <ul class="menu p-4 overflow-y-auto w-80 bg-base-100">
+        <label for="my-drawer-2" class="drawer-overlay"></label>
+        <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
           <li>
-            <a>Timeline</a>
+            <NavLink href="/feed">Feed</NavLink>
           </li>
           <li>
-            <a>Albums</a>
+            <NavLink href="/albums">Albums</NavLink>
           </li>
         </ul>
       </div>
--- frontend/solidjs/src/index.tsx
+++ frontend/solidjs/src/index.tsx
@@ -3,5 +3,13 @@ import "./index.css";
 import { render } from "solid-js/web";
 
 import App from "./App";
+import { Router } from "solid-app-router";
 
-render(() => <App />, document.getElementById("root") as HTMLElement);
+render(
+  () => (
+    <Router>
+      <App />
+    </Router>
+  ),
+  document.getElementById("root") as HTMLElement,
+);
