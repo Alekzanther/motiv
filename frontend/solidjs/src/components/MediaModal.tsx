80e042fa7e7a50bfcd728e2b19e3d6fca22c0d5c
Alexander Lindberg <alexanderlndbrg@gmail.com>
2022-06-19 23:00:43 +0200

    Prepare modal component

---
+++ frontend/solidjs/src/components/MediaModal.tsx
@@ -0,0 +1,22 @@
+import { JSXElement } from "solid-js";
+
+export type MediaModalProps = {
+  content?: JSXElement;
+};
+
+const MediaModal = (props: MediaModalProps) => {
+  return (
+    <div class="modal">
+      <div class="modal-box">
+        {props.content}
+        <h3 class="font-bold text-lg">Congratulations random Interner user!</h3>
+        <p class="py-4">
+          You've been selected for a chance to get one year of subscription to use Wikipedia for
+          free!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default MediaModal;
